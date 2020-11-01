bf72b6d5806ee826acf262ee21715e6e851ace58
molin <molinz@molins-MacBook-Pro.local>
2020-11-01 22:04:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# fdfs client for go
+
+
+original github.com/tedcy/fdfs_client
+
+contact yue.cheng.ted@gmail.com
---
+++ config.go
@@ -0,0 +1,49 @@
+package fdfs
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"runtime"
+)
+
+type Config struct {
+	TrackerAddr []string
+	MaxConns    int
+}
+
+func newConfig(configName string) (*Config, error) {
+	config := &Config{}
+	f, err := os.Open(configName)
+	if err != nil {
+		return nil, err
+	}
+	splitFlag := "\n"
+	if runtime.GOOS == "windows" {
+		splitFlag = "\r\n"
+	}
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSuffix(line, splitFlag)
+		str := strings.SplitN(line, "=", 2)
+		switch str[0] {
+		case "tracker_server":
+			config.TrackerAddr = append(config.TrackerAddr, str[1])
+		case "maxConns":
+			config.MaxConns, err = strconv.Atoi(str[1])
+			if err != nil {
+				return nil, err
+			}
+		}
+		if err != nil {
+			if err == io.EOF {
+				return config, nil
+			}
+			return nil, err
+		}
+	}
+	return config, nil
+}
